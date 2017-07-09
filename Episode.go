93e1546361236cacab03a6a0e060fc5553e2ab47
Eduard Urbach <e.urbach@gmail.com>
2017-07-09 22:48:47 +0200

    Naming fix

--- Episode.go
+++ Episode.go
@@ -2,8 +2,8 @@ package twist
 
 // Episode represents a single episode on twist.moe.
 type Episode struct {
-	Link          string `json:"link"`
-	EpisodeNumber string `json:"episode:number"`
-	AnimeTwistID  string `json:"animetwist:id"`
-	KitsuID       string `json:"kitsu:id"`
+	Link         string `json:"link"`
+	Number       string `json:"episode:number"`
+	AnimeTwistID string `json:"animetwist:id"`
+	KitsuID      string `json:"kitsu:id"`
 }
