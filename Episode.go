a16ac5ac9727cb8f7d56d201616bf4b3a7057cbd
Eduard Urbach <e.urbach@gmail.com>
2017-07-09 22:45:30 +0200

    Limited data

--- Episode.go
+++ Episode.go
@@ -2,13 +2,8 @@ package twist
 
 // Episode represents a single episode on twist.moe.
 type Episode struct {
-	Title         string `json:"title"`
-	Description   string `json:"description"`
 	Link          string `json:"link"`
-	PubDate       string `json:"pubDate"`
-	AnimeTitle    string `json:"anime:title"`
 	EpisodeNumber string `json:"episode:number"`
 	AnimeTwistID  string `json:"animetwist:id"`
 	KitsuID       string `json:"kitsu:id"`
-	MyAnimeListID string `json:"myanimelist:id"`
 }
