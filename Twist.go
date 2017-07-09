d4991953d9e564e2e1bc58d93a5bb3af5f62fb42
Eduard Urbach <e.urbach@gmail.com>
2017-07-09 16:28:24 +0200

    Implemented fetching

--- Twist.go
+++ Twist.go
@@ -1,13 +1,29 @@
 package twist
 
+import "github.com/parnurzeal/gorequest"
+
 // GetAnime queries a twist.moe anime by the twist.moe internal ID.
 // API endpoint: https://twist.moe/api/anime/:id
 func GetAnime(id string) (*Anime, error) {
-	return nil, nil
+	var anime *Anime
+	_, _, errs := gorequest.New().Get("https://twist.moe/api/anime/" + id).EndStruct(&anime)
+
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+
+	return anime, nil
 }
 
 // GetAnimeByKitsuID queries a twist.moe anime by the Kitsu ID.
 // API endpoint: https://twist.moe/api/animebykitsu/:id
 func GetAnimeByKitsuID(id string) (*Anime, error) {
-	return nil, nil
+	var anime *Anime
+	_, _, errs := gorequest.New().Get("https://twist.moe/api/animebykitsu/" + id).EndStruct(&anime)
+
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+
+	return anime, nil
 }
