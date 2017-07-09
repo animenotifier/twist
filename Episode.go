9bbb2646eba3829efc2c26449bcc191365bb6bb7
Eduard Urbach <e.urbach@gmail.com>
2017-07-09 16:21:17 +0200

    Consistent naming

--- Episode.go
+++ Episode.go
@@ -3,5 +3,5 @@ package twist
 // Episode represents a single episode on twist.moe.
 type Episode struct {
 	URL      string `json:"url"`
-	EmbedURL string `json:"embedURL"`
+	EmbedURL string `json:"embedUrl"`
 }
