3e87380d665c9b6434e20610374eb1a585dbcb2f
Eduard Urbach <e.urbach@gmail.com>
2017-07-10 17:32:29 +0200

    Removed auth

--- Twist.go
+++ Twist.go
@@ -12,7 +12,7 @@ var endpoint = "https://twist.moe/upload-feed"
 // GetFeed queries a twist.moe feed by the twist.moe internal ID.
 func GetFeed(twistID string) (*Feed, error) {
 	var anime *Feed
-	resp, _, errs := gorequest.New().SetBasicAuth("public", "not").Get(endpoint + "?format=json&animeId=" + twistID).EndStruct(&anime)
+	resp, _, errs := gorequest.New().Get(endpoint + "?format=json&animeId=" + twistID).EndStruct(&anime)
 
 	if resp.StatusCode == http.StatusNotFound {
 		return nil, errors.New("Doesn't exist on twist.moe")
@@ -28,7 +28,7 @@ func GetFeed(twistID string) (*Feed, error) {
 // GetFeedByKitsuID queries a twist.moe feed by the Kitsu ID.
 func GetFeedByKitsuID(kitsuID string) (*Feed, error) {
 	var anime *Feed
-	resp, _, errs := gorequest.New().SetBasicAuth("public", "not").Get(endpoint + "?format=json&kitsuId=" + kitsuID).EndStruct(&anime)
+	resp, _, errs := gorequest.New().Get(endpoint + "?format=json&kitsuId=" + kitsuID).EndStruct(&anime)
 
 	if resp.StatusCode == http.StatusNotFound {
 		return nil, errors.New("Doesn't exist on twist.moe")
