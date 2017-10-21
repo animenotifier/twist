743481a0ebff8589230c5db5141959337c9f0c6a
Eduard Urbach <e.urbach@gmail.com>
2017-10-21 17:27:18 +0200

    Switched to aerogo/http client

--- Twist.go
+++ Twist.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/parnurzeal/gorequest"
+	"github.com/aerogo/http/client"
 )
 
 var endpointEpisodes = "https://twist.moe/feed/episodes"
@@ -13,47 +13,35 @@ var endpointAnime = "https://twist.moe/feed/anime"
 // GetFeed queries a twist.moe feed by the twist.moe internal ID.
 func GetFeed(twistID string) (*Feed, error) {
 	var anime *Feed
-	resp, _, errs := gorequest.New().Get(endpointEpisodes + "?format=json&animeId=" + twistID).EndStruct(&anime)
+	resp, err := client.Get(endpointEpisodes + "?format=json&animeId=" + twistID).EndStruct(&anime)
 
-	if resp.StatusCode == http.StatusNotFound {
+	if resp.StatusCode() == http.StatusNotFound {
 		return nil, errors.New("Doesn't exist on twist.moe")
 	}
 
-	if len(errs) > 0 {
-		return nil, errs[0]
-	}
-
-	return anime, nil
+	return anime, err
 }
 
 // GetFeedByKitsuID queries a twist.moe feed by the Kitsu ID.
 func GetFeedByKitsuID(kitsuID string) (*Feed, error) {
 	var anime *Feed
-	resp, _, errs := gorequest.New().Get(endpointEpisodes + "?format=json&kitsuId=" + kitsuID).EndStruct(&anime)
+	resp, err := client.Get(endpointEpisodes + "?format=json&kitsuId=" + kitsuID).EndStruct(&anime)
 
-	if resp.StatusCode == http.StatusNotFound {
+	if resp.StatusCode() == http.StatusNotFound {
 		return nil, errors.New("Doesn't exist on twist.moe")
 	}
 
-	if len(errs) > 0 {
-		return nil, errs[0]
-	}
-
-	return anime, nil
+	return anime, err
 }
 
 // GetAnimeIndex queries information about all anime on twist.moe.
 func GetAnimeIndex() (*AnimeIndex, error) {
 	var animeList *AnimeIndex
-	resp, _, errs := gorequest.New().Get(endpointAnime + "?format=json").EndStruct(&animeList)
+	resp, err := client.Get(endpointAnime + "?format=json").EndStruct(&animeList)
 
-	if resp.StatusCode == http.StatusNotFound {
+	if resp.StatusCode() == http.StatusNotFound {
 		return nil, errors.New("Doesn't exist on twist.moe")
 	}
 
-	if len(errs) > 0 {
-		return nil, errs[0]
-	}
-
-	return animeList, nil
+	return animeList, err
 }
